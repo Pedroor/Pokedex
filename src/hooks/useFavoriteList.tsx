9152eab8e8467feb399fa9deaf40fb9640645441
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-04-13 07:47:13 -0300

    refactor: Flux of favoriteList

--- src/hooks/useFavoriteList.tsx
+++ src/hooks/useFavoriteList.tsx
@@ -8,6 +8,7 @@ import React, {
 import {showMessage} from 'react-native-flash-message';
 import AsyncStorage from '@react-native-community/async-storage';
 import {Alert} from 'react-native';
+import {PokemonResponse} from '../types/pokemon';
 
 type FavoriteListProviderProps = {
   children: ReactNode;
@@ -19,7 +20,24 @@ export const FavoriteListContext = createContext<FavoriteListContextData>(
   {} as FavoriteListContextData,
 );
 
-export function FavoriteHeroesProvider({children}: FavoriteListProviderProps) {
+export function FavoriteListProvider({children}: FavoriteListProviderProps) {
+  const [favoriteList, setFavoriteList] = useState<PokemonResponse[]>([]);
+
+  async function setStore(favoritePokemon: PokemonResponse[]) {
+    await AsyncStorage.setItem(
+      '@FavoriteList',
+      JSON.stringify(favoritePokemon),
+    );
+  }
+
+  async function getStore() {
+    let pokemonList = await AsyncStorage.getItem('@FavoriteList');
+    if (pokemonList) {
+      setFavoriteList(JSON.parse(pokemonList));
+    } else {
+      return;
+    }
+  }
   return (
     <FavoriteListContext.Provider value={{}}>
       {children}
